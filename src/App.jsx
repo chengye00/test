daac240084230ae31b5d7701a2079c870a434dd9
chengye00 <chengye00@users.noreply.github.com>
2025-05-14 19:38:24 +0800

    Initial commit for new project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Message from './Message';
+
+function App() {
+  return (
+    <>
+      <Message />
+    </>
+  );
+}
+
+export default App;
